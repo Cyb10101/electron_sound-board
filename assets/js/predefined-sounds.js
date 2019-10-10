60f13ea2a2f1dc7feb1b74517a2d70f4ba30caeb
Cyb10101 <cyb10101@gmail.com>
2019-10-10 14:12:02 +0200

    New sounds added

--- assets/js/predefined-sounds.js
+++ assets/js/predefined-sounds.js
@@ -4,12 +4,21 @@ class PredefinedSounds {
     getSounds() {
         return {
             'ba-da-dum': {
+                name: 'Ba Da Dum',
                 sound: 'ba-da-dum.wav',
                 icon: 'fas fa-drum',
                 soundLicence: this.getLicence('CC BY 3.0', 'Simon_Lacelle', 'https://freesound.org/people/Simon_Lacelle/sounds/37215/'),
                 imageLicence: this.getLicence('Font Awesome Free')
             },
+            'sad-trombone-01': {
+                name: 'Sad Trombone 01',
+                sound: 'sad-trombone-01.wav',
+                // icon: 'fas fa-drum',
+                soundLicence: this.getLicence('CC BY 3.0', 'Benboncan', 'https://freesound.org/people/Benboncan/sounds/73581/'),
+                imageLicence: this.getLicence('Font Awesome Free')
+            },
             'weapon-science-fiction-01': {
+                name: 'Weapon Science Fiction 01',
                 sound: 'weapon-science-fiction-01.mp3',
                 image: 'weapon-military-01.svg',
                 soundLicence: this.getLicence('ZapSplat: Standard License', 'ZapSplat', 'https://www.zapsplat.com/music/science-fiction-weapon-gun-shoot-powerful-1/'),
